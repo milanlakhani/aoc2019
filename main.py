2c1b9c38922166aa7a4f51f4cc6f0ea3060ffe63
milanlakhani <milan.lakhani@codethink.co.uk>
2019-12-17 19:50:05 +0000

    Time solution set!

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+start_time = time.time()
+import Day1
+import Day2
+# import Day3
+## Day 3 gives a memory error atm
+import Day4
+import Day5
+import Day6
+# import Day7
+import Day8
+import Day10
+# import Day12
+# import Day14
+end_time = time.time()
+print(end_time-start_time)
